f8bb3dd250668699f3ebda2903039537f813554d
Jorge Gabriel Rodrigues <jorgegabriel99h@gmail.com>
2022-03-06 11:31:10 -0300

    feat: First Commit 🎉

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace servidor_http_simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var servidorHttp = new ServidorHttp();
+        }
+    }
+}
